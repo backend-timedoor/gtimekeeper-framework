628ad1726fdde856a73cdde5e7af27a2a70c24fd
Edwin Diradinata <edwindiradinata@gmail.com>
2024-06-27 15:44:47 +0800

    mod(migration): update migration and force and fresh

--- utils/helper/string.go
+++ utils/helper/string.go
@@ -1,6 +1,10 @@
 package helper
 
-import "github.com/iancoleman/strcase"
+import (
+	"strings"
+
+	"github.com/iancoleman/strcase"
+)
 
 func ToSnakeCase(v string) string {
 	return strcase.ToKebab(v)
@@ -12,4 +16,24 @@ func ToCamelCase(v string) string {
 
 func ToPascalCase(v string) string {
 	return strcase.ToCamel(v)
+}
+
+func Pluralize(word string) string {
+	if strings.HasSuffix(word, "y") {
+		// If the word ends in "y", replace "y" with "ies"
+		return word[:len(word)-1] + "ies"
+	} else if strings.HasSuffix(word, "s") || strings.HasSuffix(word, "sh") || strings.HasSuffix(word, "ch") || strings.HasSuffix(word, "x") || strings.HasSuffix(word, "z") {
+		// For words ending in s, sh, ch, x, z, add "es"
+		return word + "es"
+	} else if strings.HasSuffix(word, "f") || strings.HasSuffix(word, "fe") {
+		// If the word ends in "f" or "fe", replace "f" or "fe" with "ves"
+		if strings.HasSuffix(word, "fe") {
+			return word[:len(word)-2] + "ves"
+		} else {
+			return word[:len(word)-1] + "ves"
+		}
+	} else {
+		// For most cases, just add "s"
+		return word + "s"
+	}
 }
